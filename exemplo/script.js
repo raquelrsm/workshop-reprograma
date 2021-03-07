9a3616814b894ca6e7369b8e968bbcf9dfefde8d
raquelrsm <quelfisica@gmail.com>
2021-03-07 14:30:14 -0300

    Primeiro commit do primeiro projeto da reprograma realizado no Workshop

---
+++ exemplo/script.js
@@ -0,0 +1,24 @@
+//Variáveis Globais
+var entrada = document.querySelector("input")
+
+var saida = document.querySelector("#respostaAno")
+
+
+function cliquei () {
+    var ano = entrada.value
+    saida.textContent = ano
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
